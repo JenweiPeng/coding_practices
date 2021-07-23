9323296ba553cc89011bd545bfa5f337b4068312
jenwei peng <jenwei.peng@gmail.com>
2021-07-22 23:24:49 -0700

    branch sum with binary tree

---
+++ branch_sums/README.md
@@ -0,0 +1,8 @@
+given a binary tree, return a list of its branch sums ordered from the leftmost branch to rightmost branch.
+
+               1
+            2    3
+          4   5 6  7
+        8  9 10
+return
+[15, 16, 18, 10, 11]
---
+++ branch_sums/branch_sums.py
@@ -0,0 +1,28 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def branchSums(root):
+        output = []
+        calculateBranchSum(root, 0, output)
+        return output
+
+    def calculateBranchSum(node, branchSum, output):
+        if node is None:
+            # empty binary tree
+            return
+        
+        branchSum += node.value
+        if node.left is None and node.right is None:
+            # no more children to continue
+            # add branch sum to the array
+            # then return back to the upper level
+            output.append(branchSum)
+            return
+
+        # traverse to the end of the left branch
+        calculateBranchSum(node.left, branchSum, output)
+        # ... to the right branch
+        calculateBranchSum(node.right, branchSum, output)
