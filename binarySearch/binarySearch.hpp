c25658bbd122af85a16dfbf3bfe5250f4e6cc0ba
Jenwei Peng <jenweipeng@Jenweis-MacBook-Pro.local>
2022-01-25 16:41:34 -0800

    add solution for binary search leetcode - 607

---
+++ binarySearch/binarySearch.hpp
@@ -0,0 +1,13 @@
+//
+//  binarySearch.hpp
+//  
+//
+//  Created by Jenwei Peng on 1/25/22.
+//
+
+#ifndef binarySearch_hpp
+#define binarySearch_hpp
+
+#include <stdio.h>
+
+#endif /* binarySearch_hpp */
